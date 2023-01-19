9c1cb60d531e256e03ceae1008630661abb902f2
Ahmed Moussa <asa30@hw.ac.uk>
2023-01-19 22:04:20 +0400

    world partially implemented

--- Screens/World.js
+++ Screens/World.js
@@ -1,13 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Dimensions, StyleSheet } from "react-native";
 import { ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
 export default () => {
+  const [myLocation, setLocation] = useState({
+    latitude: 25.101969,
+    longitude: 55.162172,
+    latitudeDelta: 0.009,
+    longitudeDelta: 0.009,
+  });
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setLocation({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.009,
+      longitudeDelta: 0.009,
+    });
+  };
+
+  useEffect(() => {userLocation()},[]);
+  
   return (
-      <SafeAreaView className="bg-zinc-900 h-full">
-        <ScrollView>
-          <Text className="text-white">This is the world page</Text>
-        </ScrollView>
-      </SafeAreaView>
+    <SafeAreaView>
+      <ScrollView>
+        <MapView style={styles.map} region={myLocation} />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
