5230855b106aa29bda0ec63ed67b8887f00c0eb2
Kanaga2004 <kc2024@hw.ac.uk>
2023-01-24 19:54:56 +0400

    added a chat layout

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -1,13 +1,91 @@
-import * as React from "react";
+// import React, { useState,useCallback, useEffect } from "react";
+// import { Button, ScrollView, Text, Image, StyleSheet } from "react-native";
+// import { useNavigation } from "@react-navigation/native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import { myFireBase } from "../fireBaseConfig";
+// import {
+//   getFirestore,
+//   collection,
+//   getDocs,
+//   getDoc,
+//   doc,
+// } from "firebase/firestore";
+// import { getStorage, ref, getDownloadURL } from "firebase/storage";
+// import { getAuth } from "firebase/auth";
+// import { useSelector } from "react-redux";
+// import { GiftedChat } from 'react-native-gifted-chat'
+
+// export function Example() {
+//   const [messages, setMessages] = useState([]);
+
+//   useEffect(() => {
+//     setMessages([
+//       {
+//         _id: 1,
+//         text: 'Hello developer',
+//         createdAt: new Date(),
+//         user: {
+//           _id: 2,
+//           name: 'React Native',
+//           avatar: 'https://placeimg.com/140/140/any',
+//         },
+//       },
+//     ])
+//   }, [])
+
+//   const onSend = useCallback((messages = []) => {
+//     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+//   }, [])
+
+//   return (
+//     <GiftedChat
+//       messages={messages}
+//       onSend={messages => onSend(messages)}
+//       user={{
+//         _id: 1,
+//       }}
+//     />
+//   )
+// }
+
+import React, { useState,useCallback, useEffect } from "react";
 import { ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { myFireBase } from "../fireBaseConfig";
+import { getAuth } from "firebase/auth";
+import { GiftedChat } from 'react-native-gifted-chat'
 
 export default () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
   return (
-      <SafeAreaView className="bg-zinc-900 h-full">
-        <ScrollView>
-          <Text className="text-white">This is the chat page</Text>
-        </ScrollView>
-      </SafeAreaView>
-  );
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 };
+
+
