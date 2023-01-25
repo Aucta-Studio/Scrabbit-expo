0d6e921aeb921a977a30eab9d09bb6e88ddc3db2
Devansh Bhatt <db2017@hw.ac.uk>
2023-01-26 00:30:44 +0400

    You can now pick images from your gallery

--- Screens/Camera.js
+++ Screens/Camera.js
@@ -1,10 +1,12 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [image, setImage] = useState(null);
 
   if (!permission) {
     // Camera permissions are still loading
@@ -25,6 +27,19 @@ export default function App() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} type={type}>
@@ -32,8 +47,12 @@ export default function App() {
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={pickImage}>
+            <Text style={styles.text}>Pick Image</Text>
+          </TouchableOpacity>
         </View>
       </Camera>
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200, position: 'absolute', bottom: 0, right: 0 }} />}
     </View>
   );
 }
