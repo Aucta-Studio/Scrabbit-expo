df9ae88e280c2a85509b6c382910f5bf588584ed
Farheen <farheen.badubhai@gmail.com>
2023-01-26 16:54:27 +0400

    chat fixed

--- fireBaseConfig.js
+++ fireBaseConfig.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfNf-vYuWKwdSNwWteBYIvH1p8hBbveMs",
@@ -11,3 +13,5 @@ const firebaseConfig = {
 };
 
 export const myFireBase = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore();
