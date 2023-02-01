51cab7c879f3364e60c57148d6f2f5f46bed4d1c
Nikhil Naik <n2000@hw.ac.uk>
2023-02-02 03:02:39 +0400

    styling for feed done

--- Screens/MyScrapbooks.js
+++ Screens/MyScrapbooks.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { ScrollView, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Post from "../Components/Post";
 
 export default () => {
   return (
