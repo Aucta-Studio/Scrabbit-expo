685425b5b7aadb20f110af54660ccfaef7765908
Ahmed Moussa <asa30@hw.ac.uk>
2023-02-12 18:23:32 +0400

    user component half done

--- Components/User.js
+++ Components/User.js
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import React from "react";
+import { myFireBase } from "../fireBaseConfig";
+import { getFirestore, collection, doc } from "firebase/firestore";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
+import { useDownloadURL } from "react-firebase-hooks/storage";
+import { useState } from "react";
+import { getStorage, ref } from "firebase/storage";
+import Firemage from "./Firemage";
 
-export default function User({id}) {
+export default function User({ id }) {
+  const [thisImg, setThisimg] = useState(null);
+  const [followed, setFollowed] = useState(null);
+  const db = getFirestore(myFireBase);
+  const storage = getStorage(myFireBase);
+
+  const [value, loading, error] = useDocument(doc(db, "Profiles", `${id}`));
+
   return (
-    <View>
-      <Text>User: {id}</Text>
-    </View>
-  )
+    <TouchableOpacity>
+      <View>
+        {error && <Text>Error: {JSON.stringify(error)}</Text>}
+        {loading && <Text>Document: Loading...</Text>}
+        {value && (
+          <>
+            <Firemage style={styles.img} path={value.data().Pfp}/>
+            <Text>{value.data().UserName}</Text>
+            <TouchableOpacity><Text>F</Text></TouchableOpacity>
+          </>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create(
+  {
+    img:{
+      width:100,
+      height:100
+    }
+  }
+);
--- Screens/Following.js
+++ Screens/Following.js
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import { myFireBase } from "../fireBaseConfig";
+import { useSelector } from "react-redux";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { useState } from "react";
+import User from "../Components/User";
 
 export default function Following() {
+  const [followed, setFolowed] = useState(null);
+  const db = getFirestore(myFireBase);
+  const auth = getAuth(myFireBase);
+  const account = useSelector((state) => state.account);
+  const relations = collection(db, "Relations");
+  const q = query(
+    relations,
+    where("Follower", "==", `${auth.currentUser.uid}`)
+  );
+  const getFollowed = async () => {
+    const temp = await getDocs(q);
+    const array = [];
+    temp.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+      array.push(doc.data().Followed);
+    });
+    setFolowed(array);
+  };
+
+  useEffect(() => {
+    getFollowed();
+  }, []);
+
+  console.log(followed);
   return (
-    <View>
-      <Text>Following</Text>
-    </View>
-  )
+    <SafeAreaView>
+      {followed?.map((followed) => {
+        return <User id={followed} />;
+      })}
+    </SafeAreaView>
+  );
 }
