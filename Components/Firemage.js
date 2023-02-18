f2b5709321ad51dbcf6fb88729cdeb9574ff6bad
Ahmed Moussa <asa30@hw.ac.uk>
2023-02-18 21:46:24 +0400

    styling pass down, paramaters pass down for ff

--- Components/Firemage.js
+++ Components/Firemage.js
@@ -4,23 +4,14 @@ import { getStorage, ref } from "firebase/storage";
 import { myFireBase } from "../fireBaseConfig";
 import { useDownloadURL } from "react-firebase-hooks/storage";
 
-export default function Firemage({ path }) {
+export default function Firemage({ path, style }) {
   const storage = getStorage(myFireBase);
   const [value, loading, error] = useDownloadURL(ref(storage, `${path}`));
   return (
     <View>
       {error && <Text>Error: {error}</Text>}
       {loading && <Text>Download URL: Loading...</Text>}
-      {!loading && value && <Image style={styles.img} source={{ uri: value }}/>}
+      {!loading && value && <Image style={style} source={{ uri: value }}/>}
     </View>
   );
 }
-
-const styles = StyleSheet.create(
-    {
-      img:{
-        width:100,
-        height:100
-      }
-    }
-  );
---
+++ Screens/ForeignProfile.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ForeignProfile({route}) {
+  return (
+    <View>
+      <Text>ForeignProfile</Text>
+    </View>
+  )
+}
