87367abdb9d90cdd67b052c61b169bb5d34ab459
Ahmed Moussa <asa30@hw.ac.uk>
2023-02-21 01:15:19 +0400

    init

--- Screens/Plant.js
+++ Screens/Plant.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Notifications() {
+export default function Plant() {
   return (
     <View>
-      <Text>Notifications</Text>
+      <Text>Plant</Text>
     </View>
   )
 }
