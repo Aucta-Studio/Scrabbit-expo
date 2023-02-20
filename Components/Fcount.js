209a5fc8fd3bfe66ca888d3df1e99542995c9d83
Ahmed Moussa <asa30@hw.ac.uk>
2023-02-21 01:15:19 +0400

    init

--- Components/Fcount.js
+++ Components/Fcount.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Notifications() {
+export default function Fcount({ id }) {
   return (
     <View>
-      <Text>Notifications</Text>
+      <Text>Fcount</Text>
     </View>
   )
 }
