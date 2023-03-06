925d9bee5f52eadc870b3b335277ff9e098ec828
Ahmed Moussa <asa30@hw.ac.uk>
2023-03-06 15:56:06 +0400

    a little fix

--- Screens/Following.js
+++ Screens/Following.js
@@ -1,5 +1,5 @@
 import { SafeAreaView, ScrollView } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { myFireBase } from "../fireBaseConfig";
 import { useSelector } from "react-redux";
 import {
@@ -10,7 +10,6 @@ import {
   where,
 } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { useState } from "react";
 import User from "../Components/User";
 
 export default function Following({ route }) {
