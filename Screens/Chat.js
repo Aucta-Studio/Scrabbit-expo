2236a8e7899e3226f62bee9db0983df1bd6fbce7
Kanaga2004 <kc2024@hw.ac.uk>
2023-03-15 19:44:01 +0400

    key error

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -3,7 +3,6 @@ import { GiftedChat } from 'react-native-gifted-chat';
 import { db } from '../fireBaseConfig';
 import { collection, query, where, orderBy, limit, addDoc, onSnapshot } from 'firebase/firestore';
 import { useSelector } from "react-redux";
-
 export default function Chat({ route }) {
   const { user } = route.params;
   const [messages, setMessages] = useState([]);
@@ -14,70 +13,50 @@ export default function Chat({ route }) {
     const q = query(
       collection(db, 'Chats'),
       orderBy('createdAt', 'desc'),
-      // where('users', 'array-contains', account.username),
-      // where('users', 'array-contains', user.UserName)
+   //   where('users', 'array-contains', account.username, user.UserName)
     );
-    const messageList = [];
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const messageList = [];
       querySnapshot.forEach((doc) => {
-      const message = {
+    const message = {
       ...doc.data(),
       createdAt: new Date(doc.data().createdAt.seconds * 1000),
       _id: doc.id,
     };
     messageList.push(message);
   });
-
-        setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, messageList)
-      );
+  setMessages(previousMessages =>
+    GiftedChat.append(previousMessages, messageList)
+  );
     });
+
 
     return () => unsubscribe();
   }, []);
 
 
-
-  const handleSend = async (newMessages = []) => {
-    const { text } = newMessages[0];
-  
+  const handleSend = async (newMessage) => {
     try {
       const docRef = await addDoc(collection(db, 'Chats'), {
-        text,
-        user: {
-          _id: account.username,
-        },
-        users: [user.UserName, account.username],
+        text: newMessage[0].text,
         createdAt: new Date(),
+        users: [account.username, user.UserName],
+        user: {
+          _id:account.username,
+          name: account.firstname,
+        },
       });
-  
-      const newMessage = {
-        text,
-        createdAt: new Date(),
-        user: {
-          _id: account.username,
-          name: account.username,
-        },
-      };
-  
-      setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, newMessage),
-      );
     } catch (error) {
       console.error(error);
     }
   };
-
-  
-  
-  
 
   return (
     <GiftedChat
       messages={messages}
       onSend={(newMessage) => handleSend(newMessage)}
       user={{
-        _id: account.username,
+        _id:account.username,
         name: account.firstname,
       }}
     />
