65864193ff024d1f1b9f56cdd12f590e0f5738ef
Ahmed Moussa <asa30@hw.ac.uk>
2023-03-16 22:46:25 +0400

    added darkmode to settings

--- store.js
+++ store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk';
 import accountReducer from './Slices/account/accountSlice'
+import settingReducer from './Slices/setting/settingSlice';
 
 export const store = configureStore({
   reducer: {
     account: accountReducer,
+    setting: settingReducer,
   },
   middleware: [thunk]
 })
