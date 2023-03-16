1a94f56680c41c6c845b5980c392f211b03c8b43
Ahmed Moussa <asa30@hw.ac.uk>
2023-03-16 22:46:35 +0400

    do better nikhil

--- Components/Carrot.js
+++ Components/Carrot.js
@@ -10,43 +10,64 @@ import { useDocument } from "react-firebase-hooks/firestore";
 import { doc } from "firebase/firestore";
 import { db } from "../fireBaseConfig";
 
-export default function Carrot({ coordinate, title, userID, dateOC, saves, likes, comments}) {
-  const timestamp = new Date(dateOC?.seconds * 1000 + dateOC?.nanoseconds / 1000000);
-  const date = timestamp.toLocaleDateString('en-GB');
+export default function Carrot({
+  coordinate,
+  title,
+  userID,
+  dateOC,
+  saves,
+  likes,
+  comments,
+}) {
+  const timestamp = new Date(
+    dateOC?.seconds * 1000 + dateOC?.nanoseconds / 1000000
+  );
+  const date = timestamp.toLocaleDateString("en-GB");
   const url = `https://www.google.com/maps/search/?api=1&query=${coordinate.latitude},${coordinate.longitude}`;
   // const d = doc();
   const [value, loading, error] = useDocument(doc(db, "Profiles", `${userID}`));
   return (
     <Marker coordinate={coordinate} image={carrot}>
-      <Callout borderRadius={10} tooltip={true} style={styles.callout} overflow={"hidden"} onPress={()=>{Linking.openURL(url);}}>
-        <View>
+      <Callout
+        // borderRadius={10}
+        tooltip
+        style={styles.callout}
+        overflow={"hidden"}
+        onPress={() => {
+          Linking.openURL(url);
+        }}
+      >
+        <View style={styles.container}>
           <Text style={styles.margins}>
             {/* <Firemage style={styles.img} path='Pfps/default.jpg' /> */}
             <Icon name="ios-person-circle" style={styles.img} />
-            <Text style={styles.username}>{value?.data()?.UserName} • {date}</Text>
+            <Text style={styles.username}>
+              {value?.data()?.UserName} • {date}
+            </Text>
           </Text>
 
           <Text style={styles.title}>{title}</Text>
-            <View flex-direction={"row"} justifyContent={"space-around"}alignItems={"stretch"}>
-              
-              <Text style={styles.countn}>
-              <Icon
-                    name={"eye"}
-                    size={34}
-                    color={"#000"}
-                  />{saves}</Text>
-              <Text style={styles.countn}>
-              <Icon
-                name={"heart"}
-                size={34}
-                color={"#000"}
-              />{likes}</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <View style={styles.stats}>
+              <Text style={styles.countn}>{saves}</Text>
+              <Icon name={"eye"} size={34} color={"#000"} />
             </View>
-          {comments && <Text>Comments: {comments}</Text>}
-        
-          <TouchableOpacity alignItems={"center"}><Text style={styles.mapgo} >
-          <Icon name="ios-navigate-circle" size={34} color="#000" />
-          </Text></TouchableOpacity>
+            <View style={styles.stats}>
+              <Icon name={"heart"} size={34} color={"#000"} />
+              <Text style={styles.countn}>{likes}</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity alignItems={"center"}>
+            <Text style={styles.mapgo}>
+              <Icon name="map-outline" size={34} color="#000" />
+            </Text>
+          </TouchableOpacity>
         </View>
       </Callout>
     </Marker>
@@ -58,36 +79,47 @@ const styles = StyleSheet.create({
     width: 32,
     height: 32,
   },
-  title:{
+  title: {
     fontSize: 25,
     fontWeight: "bold",
     color: "#000",
   },
-  username:{
+  username: {
     fontSize: 13,
     color: "#000",
-
   },
-  callout:{
+  callout: {
     width: 250,
     height: 250,
-    padding: '5%',
-    borderColor: "#AAA",
-    backgroundColor: "#EC6319",
+    padding: "5%",
   },
-  margins:{
+  container:{
+    width: 200,
+    height: 200,
+    padding: "5%",
+    backgroundColor: "#EC6319",
+    borderColor: "#AAA",
+    alignSelf: "center",
+    justifyContent: "center",
+    marginTop: "15%",
+    borderRadius: 30
+  },
+  margins: {
     marginTop: -5,
     marginBottom: -5,
   },
-  countn:{
+  countn: {
     fontSize: 34,
   },
-  mapgo:{
+  mapgo: {
     fontSize: 34,
     fontWeight: "bold",
-    textAlign:"center",
-    marginTop:50
-  }
-
-
+    textAlign: "center",
+    marginTop: "5%",
+  },
+  stats: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
 });
