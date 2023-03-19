8a0d91c42aecc8d0daee3000b69be6b2ff96fd29
Ahmed Moussa <asa30@hw.ac.uk>
2023-03-19 21:20:48 +0400

    chat fixed

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -1,64 +1,105 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
-import { db } from '../fireBaseConfig';
-import { collection, query, where, orderBy, limit, addDoc, onSnapshot } from 'firebase/firestore';
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import { auth, db } from "../fireBaseConfig";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
+  addDoc,
+  onSnapshot,
+  arrayUnion,
+  getDocs,
+} from "firebase/firestore";
 import { useSelector } from "react-redux";
 export default function Chat({ route }) {
   const { user } = route.params;
+  const [chatID, setChatID] = useState([]);
   const [messages, setMessages] = useState([]);
   const account = useSelector((state) => state.account);
+  const me = {
+    _id: auth.currentUser.uid,
+    name: account.firstname,
+  }
+  const qc = query(
+    collection(db, "Chats"),
+    where("users", "==", [auth.currentUser.uid, user])
+  );
 
+  async function getChatID() {
+    const temp = await getDocs(qc);
+    let array = [];
+    temp.forEach((doc) => {
+      array.push(doc.id);
+    });
+    console.log("array of docs", array);
+    if (array.length > 0) {
+      // Chat already exists, set chat ID to existing chat ID
+      setChatID(array[0]);
+      return array[0];
+    } else {
+      // Create new chat
+      const newChat = addDoc(collection(db, "Chats"), {
+        users: arrayUnion(auth.currentUser.uid, user),
+      });
+      setChatID(newChat.id);
+      return newChat.id;
+    }
+  }
+
+  async function getMessages() {
+    const chat = await getChatID();
+    const messageRef = query(collection(db, "Chats", chat, "Messages"),orderBy("createdAt", "desc"));
+    const temp = onSnapshot(messageRef, (querySnapshot) => {
+      setMessages(
+        querySnapshot.docs.map((doc) => ({
+          _id: doc.id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+        }))
+      );
+    });
+  }
 
   useEffect(() => {
-    const q = query(
-      collection(db, 'Chats'),
-      orderBy('createdAt', 'desc'),
-   //   where('users', 'array-contains', account.username, user.UserName)
-    );
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const messageList = [];
-      querySnapshot.forEach((doc) => {
-    const message = {
-      ...doc.data(),
-      createdAt: new Date(doc.data().createdAt.seconds * 1000),
-      _id: doc.id,
-    };
-    messageList.push(message);
-  });
-  setMessages(previousMessages =>
-    GiftedChat.append(previousMessages, messageList)
-  );
-    });
-
-
-    return () => unsubscribe();
+    getMessages();
   }, []);
 
+  // {chatID && !messages && getMessages()}
 
-  const handleSend = async (newMessage) => {
+  const handleSend = useCallback(async (newMessages = []) => {
     try {
-      const docRef = await addDoc(collection(db, 'Chats'), {
-        text: newMessage[0].text,
+      const messagesRef = collection(db, 'Chats', chatID, 'Messages');
+      const newMessage = newMessages[0];
+      const { text } = newMessage;
+  
+      const docRef = await addDoc(messagesRef, {
+        text,
         createdAt: new Date(),
-        users: [account.username, user.UserName],
         user: {
-          _id:account.username,
+          _id: auth.currentUser.uid,
           name: account.firstname,
         },
       });
+  
+      console.log('Message added:', docRef.id);
+  
     } catch (error) {
       console.error(error);
     }
-  };
+  }, [account.firstname, chatID]);
+
+  console.log("chatID: ", chatID);
+  console.log("messages: ", messages);
 
   return (
     <GiftedChat
       messages={messages}
+      inverted={true}
       onSend={(newMessage) => handleSend(newMessage)}
-      user={{
-        _id:account.username,
-        name: account.firstname,
-      }}
+      user={me}
     />
   );
 }
--- Screens/ChatList.js
+++ Screens/ChatList.js
@@ -1,46 +1,63 @@
-import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native';
-import React, { useEffect, useState} from "react";
-import { db } from "../fireBaseConfig";
-import { collection, query, where, getDocs, getDoc  } from "firebase/firestore";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+import { db, auth } from "../fireBaseConfig";
+import { collection, query, where, getDocs, getDoc } from "firebase/firestore";
 import Profile from "./Profile";
 import { useSelector } from "react-redux";
-import Chat from "./Chat"
+import Chat from "./Chat";
 import { useNavigation } from "@react-navigation/native";
-import Firemage from '../Components/Firemage';
+import Firemage from "../Components/Firemage";
+import ChatUser from "../Components/ChatUser";
+import { ScrollView } from "react-native";
+import { RefreshControl } from "react-native";
 
 export default function ChatList() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
   const account = useSelector((state) => state.account);
   const navigation = useNavigation();
-
+  const fetchUsers = async () => {
+    try {
+      const q = query(
+        collection(db, "Relations"),
+        where("Follower", "==", `${auth.currentUser.uid}`)
+      );
+      const unsub = await getDocs(q);
+      let array = [];
+      unsub.forEach((doc) => {
+        array.push(doc.data().Followed);
+      });
+      console.log(array);
+      setUsers(array);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    fetchUsers();
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const q = query(collection(db, "Profiles"), where("UserName", "!=", `${account.username}`));
-        const unsub = await getDocs(q);
-        let array = [];
-        unsub.forEach((doc) => {
-          array.push(doc.data())
-        });
-        setUsers(array);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
-      }
-    };
     fetchUsers();
   }, []);
 
   return (
-    <SafeAreaView className="users_container">
-        {users?.map((user,index) => (
-          <TouchableOpacity onPress={() => navigation.navigate("Chat", {user: user})} key={index} >
-            {user && <Firemage path={user?.Pfp} style={{width:50,height:50}}/>}
-            <Text>{user.FirstName} {user.LastName}</Text>
-          </TouchableOpacity> 
-        ))} 
-    </SafeAreaView>
-  )
+    <ScrollView className="users_container" refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+      {users?.map((user, index) => (
+        <ChatUser key={index} id={user} />
+      ))}
+    </ScrollView>
+  );
 }
